1c1b333c238bf9e0700a7cadcde6756028061e7d
Remi Gau <remi_gau@hotmail.com>
2022-08-15 22:51:43 +0200

    undo

--- reproschema/models/protocol.py
+++ reproschema/models/protocol.py
@@ -1,4 +1,7 @@
+from pathlib import Path
+from typing import Dict
 from typing import Optional
+from typing import Union
 
 from attrs import define
 
@@ -14,22 +17,23 @@ class Protocol(SchemaBase):
     class to deal with reproschema protocols
     """
 
-    # name = field(default="protocol")
-
     def __init__(
         self,
         name: Optional[str] = "protocol",
-        # schemaVersion: Optional[str] = None,
-        # prefLabel: Optional[str] = "protocol",
-        # landingPage: Optional[dict] = None,
-        # citation: Optional[str] = None,
-        # image: Optional[Union[str, Dict[str, str]]] = None,
-        # audio: Optional[Union[str, Dict[str, str]]] = None,
-        # video: Optional[Union[str, Dict[str, str]]] = None,
-        # messages: Optional[Dict[str, str]] = None,
+        schemaVersion: Optional[str] = None,
+        prefLabel: Optional[str] = "protocol",
+        altLabel: Optional[Union[str, Dict[str, str]]] = None,
+        description: Optional[str] = "",
+        landingPage: Optional[dict] = None,
+        preamble: Optional[str] = None,
+        citation: Optional[str] = None,
+        image: Optional[Union[str, Dict[str, str]]] = None,
+        audio: Optional[Union[str, Dict[str, str]]] = None,
+        video: Optional[Union[str, Dict[str, str]]] = None,
+        messages: Optional[Dict[str, str]] = None,
         suffix: Optional[str] = "_schema",
         ext: Optional[str] = ".jsonld",
-        # output_dir: Optional[Union[str, Path]] = Path.cwd(),
+        output_dir: Optional[Union[str, Path]] = Path.cwd(),
         lang: Optional[str] = DEFAULT_LANG(),
         **kwargs
     ):
@@ -44,21 +48,21 @@ class Protocol(SchemaBase):
         super().__init__(
             at_id=name,
             at_type="reproschema:Protocol",
-            # schemaVersion=schemaVersion,
-            # prefLabel={lang: prefLabel},
-            # altLabel=altLabel,
-            # description=description,
-            # landingPage=landingPage,
-            # preamble=preamble,
-            # citation=citation,
-            # messages=messages,
-            # image=image,
-            # audio=audio,
-            # video=video,
+            schemaVersion=schemaVersion,
+            prefLabel={lang: prefLabel},
+            altLabel=altLabel,
+            description=description,
+            landingPage=landingPage,
+            preamble=preamble,
+            citation=citation,
+            messages=messages,
+            image=image,
+            audio=audio,
+            video=video,
             schema_order=schema_order,
             suffix=suffix,
             ext=ext,
-            # output_dir=output_dir,
+            output_dir=output_dir,
             lang=lang,
             **kwargs
         )
