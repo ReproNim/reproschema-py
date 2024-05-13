311fd1730801a1cb9103c9fb0162995deaa9485e
ibevers <isaacbevers@gmail.com>
2024-05-13 13:50:09 -0400

    Initiate pdf2reproschema

---
+++ reproschema/pdf2reproschema.py
@@ -0,0 +1,18 @@
+"""
+Converts pdfs of questionnaires to ReproSchema activities. 
+"""
+
+
+#parse pdf text
+#LLM(parsed_text, pydantic_models)
+#format validation
+#lexical match
+#feed back to LLM until converges
+#evaluate
+#if good, optionally subset
+#push to UI for check
+
+
+
+
+#modes: human-in-the-loop, maximally automated
