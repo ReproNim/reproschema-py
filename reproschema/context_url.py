d36cd91209f2183335765325b51a8f4bd505c167
djarecka <djarecka@gmail.com>
2024-06-21 16:41:29 +0000

    Add new version of the pydantic model and new context url

--- reproschema/context_url.py
+++ reproschema/context_url.py
@@ -1,1 +1,2 @@
-CONTEXTFILE_URL = "https://raw.githubusercontent.com/ReproNim/reproschema/ref/linkml/contexts/reproschema"
+# this is automatically updated after reproschema new release
+CONTEXTFILE_URL = 'https://raw.githubusercontent.com/ReproNim/reproschema/main/releases/1.0.0/reproschema'
